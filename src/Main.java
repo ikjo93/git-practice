4b7856a331bbf3a8681d1ea9d9b182b2e650454a
ikjo93 <auddlr100@naver.com>
2022-04-03 15:45:24 +0900

    update : Main 코드 로직 작성

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,10 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello Git!!");
+        int[] numbers = new int[10];
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = i;
+            System.out.println(numbers[i]);
+        }
     }
 }
