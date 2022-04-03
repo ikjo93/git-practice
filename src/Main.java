02497fcfac0f3efbfaa83a9a515f12ea8c5b75fb
ikjo93 <auddlr100@naver.com>
2022-04-03 15:57:35 +0900

    update : 수정

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello Git!!");
-        int[] numbers = new int[10];
-        for (int i = 0; i < numbers.length; i++) {
-            numbers[i] = i;
-            System.out.println(numbers[i]);
-        }
-
-        for (int i = 0; i < numbers.length; i++) {
-            numbers[i] = numbers[i]*2;
-            System.out.println(numbers[i]);
+        System.out.println("Hello Java");
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < list.size(); i++) {
+            list.add(i);
+            System.out.println(list.get(i));
         }
     }
 }
